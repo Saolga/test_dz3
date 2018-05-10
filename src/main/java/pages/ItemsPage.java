b830af71bd20b89ef8657de2f1e13e5ddcd30cf7
Saolga <38721248+Saolga@users.noreply.github.com>
2018-05-10 10:57:41 +0300

    Update ItemsPage.java

--- src/main/java/pages/ItemsPage.java
+++ src/main/java/pages/ItemsPage.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Select;
 import other.Stash;
 
 import java.util.List;
